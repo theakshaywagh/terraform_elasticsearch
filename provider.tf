3345f1f9437650b0a9ff829ea9b6d5c52fcb611b
Akshay Wagh <113848787+theakshaywagh@users.noreply.github.com>
2023-02-17 14:35:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,8 +14,8 @@ provider "elasticsearch" {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA6A2F5MKZGA64FNO5"        #change this
-  secret_key = "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"    #change this
+  access_key = "AKIA2F5MKZGAFNO5"        #change this
+  secret_key = "H/mXZ1pSC3/B1kVIt5hHN5x9xYAEO6qn6HHI"    #change this
 }
 
 
